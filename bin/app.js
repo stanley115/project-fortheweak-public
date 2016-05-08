07fe6cb56bc51bd2ac6beb25d07830cf3e3c3b52
lunglungyu <lunglungyu2004@gmail.com>
2016-05-08 15:22:27 +0800

    Coding room api

--- bin/app.js
+++ bin/app.js
@@ -3,11 +3,36 @@
  */
 module.exports = function(app){
 
+    //use the same object to store room info & client info;
+    var globalData = {};
+    globalData['client'] = {};
+    globalData['room'] = {};
     var io = require('socket.io').listen(app);
+    var client_id_pool = 0 ;
+    var room_id_pool = 0;
 
     io.on('connection', function(socket){
-
-
+      var client_id = ++client_id_pool;
+      socket.on('disconnect',function(){
+        console.log('disconnect');
+      });
+      socket.on('roomCreate',function(roomName){
+        console.log('roomCreate');
+        var room_id = ++room_id_pool;
+        globalData[room_id]['name'] = roomName;
+      });
+      socket.on('roomJoin',function(){
+        console.log('roomJoin');
+      });
+      socket.on('roomLeave',function(){
+        console.log('roomLeave');
+      });
+      socket.on('roomList',function(){
+        console.log('roomList');
+      });
+      socket.on('roomUpdate',function(){
+        console.log('roomUpdate');
+      });
 
     });
 
