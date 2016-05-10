822e0ae7f3cd33b2d83183e17d9cca2956539833
lunglungyu <lunglungyu2004@gmail.com>
2016-05-10 20:31:01 +0800

    style fix + refactor js + css + renaming for readability

--- bin-front/Welcome.js
+++ bin-front/Welcome.js
@@ -1,10 +1,8 @@
 var Welcome = function(config){
   document.getElementById("fullscreen").addEventListener("click", goFullscreen.bind(null));
-
   //fadeOut
   function fadeOut(el){
     el.style.opacity = 1;
-
     (function fade() {
       if ((el.style.opacity -= .05) < 0) {
         el.style.display = "none";
@@ -13,13 +11,10 @@ var Welcome = function(config){
       }
     })();
   }
-
   // fade in
-
   function fadeIn(el, display){
     el.style.opacity = 0;
     el.style.display = display || "block";
-
     (function fade() {
       var val = parseFloat(el.style.opacity);
       if (!((val += .05) > 1)) {
@@ -28,68 +23,60 @@ var Welcome = function(config){
       }
     })();
   }
-
   function goFullscreen() {
-      // Get the element that we want to take into fullscreen mode
-      var element = document.body;
-      //var element = document.getElementById('welcome-page')
-      console.log(element);
-      // These function will not exist in the browsers that don't support fullscreen mode yet,
-      // so we'll have to check to see if they're available before callingublic them.
-
-      if (element.mozRequestFullScreen) {
-          // This is how to go into fullscren mode in Firefox
-          // Note the "moz" prefix, which is short for Mozilla.
-          element.mozRequestFullScreen();
-      } else if (element.webkitRequestFullScreen) {
-          // This is how to go into fullscreen mode in Chrome and Safari
-          // Both of those browsers are based on the Webkit project, hence the same prefix.
-          element.webkitRequestFullScreen();
-      }
-      // Hooray, now we're in fullscreen mode!
-//      var divid = document.getElementById('welcome-div');
-//      $(divid).fadeOut("slow");
-      var ele = document.getElementById('welcome-page');
-      fadeOut(ele);
-      var ele = document.getElementById('welcome-page-background');
-      fadeIn(ele);
-  //    var ele = document.getElementById('CUHK-logo-background');
+    // Get the element that we want to take into fullscreen mode
+    var element = document.body;
+    //var element = document.getElementById('welcome-page')
+    console.log(element);
+    // These function will not exist in the browsers that don't support fullscreen mode yet,
+    // so we'll have to check to see if they're available before callingublic them.
+    if (element.mozRequestFullScreen) {
+      // This is how to go into fullscren mode in Firefox
+      // Note the "moz" prefix, which is short for Mozilla.
+      element.mozRequestFullScreen();
+    } else if (element.webkitRequestFullScreen) {
+      // This is how to go into fullscreen mode in Chrome and Safari
+      // Both of those browsers are based on the Webkit project, hence the same prefix.
+      element.webkitRequestFullScreen();
+    }
+    // Hooray, now we're in fullscreen mode!
+    //      var divid = document.getElementById('welcome-div');
+    //      $(divid).fadeOut("slow");
+    var ele = document.getElementById('welcome-page');
+    fadeOut(ele);
+    var ele = document.getElementById('welcome-page-background');
+    fadeIn(ele);
+    //    var ele = document.getElementById('CUHK-logo-background');
     //  fadeIn(ele);
-
-      setTimeout(
+    setTimeout(
         function tmp1() {
           var ele = document.getElementById('welcome-page-background');
           fadeOut(ele);
         },
-      1200);
-      setTimeout(
+        1200);
+    setTimeout(
         function tmp2() {
           var ele = document.getElementById('CUHK-logo-background');
           fadeIn(ele)
         },
-      1900);
-
-      setTimeout(
+        1900);
+    setTimeout(
         function tmp3() {
           var ele = document.getElementById('CUHK-logo-background');
           fadeOut(ele)
         },
-      3100);
-
-      setTimeout(
+        3100);
+    setTimeout(
         function tmp4() {
           var ele = document.getElementById('welcome-div');
           ele.classList.add("remove");
           document.getElementById('game-div').style.display = 'block'
         },
-      3800);
-      //$(document.getElementById('welcome-div')).fadeOut("slow");
-      //document.getElementById('welcome-div').style.display='none'
+        3800);
+    //$(document.getElementById('welcome-div')).fadeOut("slow");
+    //document.getElementById('welcome-div').style.display='none'
   }
 }
-
 Welcome.prototype.start = function(){
-
 }
-
 module.exports = Welcome
--- bin-front/main.js
+++ bin-front/main.js
@@ -1,18 +1,20 @@
 "use strict";
 
-//var Welcome = require('./Welcome');
-var Game = require('./Game');
+var Welcome = require('./Welcome');
+var welcome = new Welcome();
 
-//var welcome = new Welcome();
+/*
+var Game = require('./Game');
 var game = new Game({
   players: [{}],
     role: 0
 });
+game.start();
+*/
 
 
 var communication = require('./Communication');
 require('./StylesAndFix');
-game.start();
 
 //
 /*
