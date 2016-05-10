f83b7fbc94c7869ba3799487477996f744b012ab
lunglungyu <lunglungyu2004@gmail.com>
2016-05-10 20:02:41 +0800

    put client socket before bundle.js for game , default load game player view

--- bin-front/main.js
+++ bin-front/main.js
@@ -1,17 +1,18 @@
 "use strict";
 
-var Welcome = require('./Welcome');
-//var Game = require('./Game');
+//var Welcome = require('./Welcome');
+var Game = require('./Game');
 
-var welcome = new Welcome();
-/*var game = new Game({
-    role: 'viewer'
-});*/
+//var welcome = new Welcome();
+var game = new Game({
+  players: [{}],
+    role: 0
+});
 
 
 var communication = require('./Communication');
 require('./StylesAndFix');
-//game.start();
+game.start();
 
 //
 /*
